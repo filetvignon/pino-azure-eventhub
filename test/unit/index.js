aef53e60509b4bf552558984ec43d6f25a3e1473
ron litzenberger <ron.litzenberger@gmail.com>
2017-08-03 21:46:22 -0500

    added test layout

---
+++ test/unit/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const expect = require('code').expect
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+lab.test('returns done', (done) => {
+    
+    done();
+});
