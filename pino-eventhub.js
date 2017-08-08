ebb7b74877231e3da418a6a6fc4fe9b01e20479a
ron litzenberger <ron.litzenberger@gmail.com>
2017-08-08 11:48:59 -0500

    refactor and added eventhub tests

--- pino-eventhub.js
+++ pino-eventhub.js
@@ -57,10 +57,9 @@ function pinoEventHub (opts) {
     return function inner(response) {
       debug('response.statusCode =', response.statusCode)
       debug('response.statusMessage =', response.statusMessage)
-
       if (response.statusCode != 201) {
-        // splitter.emit(`response error =`, response.statusMessage)
-        console.log(`response error =`, response.statusMessage)
+        splitter.emit('error', new Error(response.statusCode ))
+        console.log('Error',response.statusMessage)
       }
 
       response.on('data', function (data) {
@@ -110,7 +109,6 @@ function pinoEventHub (opts) {
         const req = https.request(options, callback(done))
         req.on('error', (e) => {
           console.error(`request error =`, e)
-          done()
         })
         req.write(line)
         req.end()
@@ -120,9 +118,10 @@ function pinoEventHub (opts) {
     },
   })
 
-  pump(splitter, writable)
+  pump(splitter,writable)
 
   return splitter
+  
 }
 
 
@@ -131,72 +130,3 @@ module.exports = {
   pinoEventHub: pinoEventHub,
 }
 
-function start (opts) {
-  if (opts.help) {
-    console.log(fs.readFileSync(path.join(__dirname, './usage.txt'), 'utf8'))
-    return
-  }
-
-  if (opts.version) {
-    console.log('pino-eventhub', require('./package.json').version)
-    return
-  }
-
-  const ehn = opts['event-hub-namespace'] || process.env.PINO_EVENT_HUB_NAMESPACE
-  const eh = opts['event-hub'] || process.env.PINO_EVENT_HUB
-  const sapn = opts['shared-access-policy-name'] || process.env.PINO_SHARED_ACCESS_POLICY_NAME
-  const sapk = opts['shared-access-policy-key'] || process.env.PINO_SHARED_ACCESS_POLICY_KEY
-  const sas = opts['sas'] || process.env.PINO_SHARED_ACCESS_SIGNATURE
-
-  if (!ehn || !eh || !sapn || (!sas && !sapk) ) {
-    console.log(fs.readFileSync(path.join(__dirname, './usage.txt'), 'utf8'))
-    console.log("  1 or more missing required parameters 'event-hub-namespace', 'event-hub', 'shared-access-policy-name' and  'sas'.")
-    if (!sas) {
-      giveSecurityWarning()
-    }
-    return
-  }
-  if (opts.expiry && !Number.isNumber(opts.expiry)) {
-    console.log(`"expiry" should be in unix date format`)
-    return
-  }
-
-  const now = new Date()
-  const week = 60*60*24*7
-  const host = 'https://' + ehn + '.servicebus.windows.net'
-  //  path = eh
-  const uri = encodeURIComponent(host + '/' + eh)
-  const se = opts.expiry || process.env.PINO_SAS_EXPIRY
-    || Math.round(now.getTime() / 1000) + week
-  const options = Object.assign(opts, {
-    host,
-    eh,
-    sr: uri,
-    sig: sas || createSignature(uri, se, sapk, true),
-    se,
-    skn: sapn,
-  })
-
-  pump(process.stdin, pinoEventHub(options))
-}
-
-if (require.main === module) {
-  start(minimist(process.argv.slice(2), {
-    alias: {
-      version: 'v',
-      help: 'h',
-      'event-hub-namespace': 's',
-      'event-hub': 'e',
-      'shared-access-policy-name': 'n',
-      'shared-access-policy-key': 'k',
-      'expiry': 'x',
-      'sas': 'a',
-      'bulk-size': 'b',
-      'port': 'p',
-    },
-    default: {
-      port: 443,
-      'bulk-size': 500,
-    }
-  }))
-}
---
+++ start.js
@@ -0,0 +1,85 @@
+#! /usr/bin/env node
+'use strict'
+
+const minimist = require('minimist')
+const Writable = require('readable-stream').Writable
+const split = require('split2')
+const pump = require('pump')
+const fs = require('fs')
+const path = require('path')
+const utf8 = require('utf8')
+const crypto = require('crypto')
+const https = require('https')
+const debug = require('debug')('pino-eventhub')
+const Parse = require('fast-json-parse')
+const pinoeh = require('./pino-eventhub')
+
+function start (opts) {
+  if (opts.help) {
+    console.log(fs.readFileSync(path.join(__dirname, './usage.txt'), 'utf8'))
+    return
+  }
+
+  if (opts.version) {
+    console.log('pino-eventhub', require('./package.json').version)
+    return
+  }
+
+  const ehn = opts['event-hub-namespace'] || process.env.PINO_EVENT_HUB_NAMESPACE
+  const eh = opts['event-hub'] || process.env.PINO_EVENT_HUB
+  const sapn = opts['shared-access-policy-name'] || process.env.PINO_SHARED_ACCESS_POLICY_NAME
+  const sapk = opts['shared-access-policy-key'] || process.env.PINO_SHARED_ACCESS_POLICY_KEY
+  const sas = opts['sas'] || process.env.PINO_SHARED_ACCESS_SIGNATURE
+
+  if (!ehn || !eh || !sapn || (!sas && !sapk) ) {
+    console.log(fs.readFileSync(path.join(__dirname, './usage.txt'), 'utf8'))
+    console.log("  1 or more missing required parameters 'event-hub-namespace', 'event-hub', 'shared-access-policy-name' and  'sas'.")
+    if (!sas) {
+      giveSecurityWarning()
+    }
+    return
+  }
+  if (opts.expiry && !Number.isNumber(opts.expiry)) {
+    console.log(`"expiry" should be in unix date format`)
+    return
+  }
+
+  const now = new Date()
+  const week = 60*60*24*7
+  const host = 'https://' + ehn + '.servicebus.windows.net'
+  //  path = eh
+  const uri = encodeURIComponent(host + '/' + eh)
+  const se = opts.expiry || process.env.PINO_SAS_EXPIRY
+    || Math.round(now.getTime() / 1000) + week
+  const options = Object.assign(opts, {
+    host,
+    eh,
+    sr: uri,
+    sig: sas || pinoeh.createSignature(uri, se, sapk, true),
+    se,
+    skn: sapn,
+  })
+
+  pump(process.stdin, pinoeh.pinoEventHub(options))
+}
+
+if (require.main === module) {
+  start(minimist(process.argv.slice(2), {
+    alias: {
+      version: 'v',
+      help: 'h',
+      'event-hub-namespace': 's',
+      'event-hub': 'e',
+      'shared-access-policy-name': 'n',
+      'shared-access-policy-key': 'k',
+      'expiry': 'x',
+      'sas': 'a',
+      'bulk-size': 'b',
+      'port': 'p',
+    },
+    default: {
+      port: 443,
+      'bulk-size': 500,
+    }
+  }))
+}
