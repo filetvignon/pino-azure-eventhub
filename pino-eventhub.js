93eabcddc9c7e06500e49cb8a60113a5a2167fa7
felipe <felipe.vignon@openstartups.net>
2020-12-03 22:56:07 -0300

    wip

--- pino-eventhub.js
+++ pino-eventhub.js
@@ -1,121 +1,134 @@
 #! /usr/bin/env node
-'use strict'
+"use strict";
 
-const Writable = require('readable-stream').Writable
-const split = require('split2')
-const pump = require('pump')
-const utf8 = require('utf8')
-const crypto = require('crypto')
-const https = require('https')
-const debug = require('debug')('pino-eventhub')
-const Parse = require('fast-json-parse')
-const socketCount = 10
+const { Writable, pipeline } = require("stream");
+const split = require("split2");
+const crypto = require("crypto");
+const https = require("https");
+const debug = require("debug")("pino-eventhub");
+const Parse = require("fast-json-parse");
+const socketCount = 10;
 
-function giveSecurityWarning () {
-  console.warn('It is poor security practice to share your Shared Access Policy Key. It is better to calculate the Shared Access Signature, and share that.')
-  console.log("'pino-eventhub.createSignature' can be used to calculate the Shared Access Signature.")
+function giveSecurityWarning() {
+  console.warn(
+    "It is poor security practice to share your Shared Access Policy Key. It is better to calculate the Shared Access Signature, and share that."
+  );
+  console.log(
+    "'pino-eventhub.createSignature' can be used to calculate the Shared Access Signature."
+  );
 }
 
-function createSignature (uri, ttl, sapk, warn) {
+function createSignature(uri, ttl, sapk, warn) {
   if (warn) {
-    giveSecurityWarning()
+    giveSecurityWarning();
   }
 
-  const signature = uri + '\n' + ttl
-  const signatureUTF8 = utf8.encode(signature)
-  const hash = crypto.createHmac('sha256', sapk)
-    .update(signatureUTF8)
-    .digest('base64')
-  return encodeURIComponent(hash)
+  const signature = uri + "\n" + ttl;
+  const hash = crypto
+    .createHmac("sha256", sapk)
+    .update(signature)
+    .digest("base64");
+  return encodeURIComponent(hash);
 }
 
-function pinoEventHub (opts) {
+function pinoEventHub(opts) {
   const splitter = split(function (line) {
-    return line
-  })
-  const agent = new https.Agent({keepAlive: true, maxSockets: opts.max || socketCount})
+    return line;
+  });
+  const agent = new https.Agent({
+    keepAlive: true,
+    maxSockets: opts.max || socketCount,
+  });
   const options = {
-    method: 'POST',
+    method: "POST",
     host: opts.host.slice(8), // remove 'https://'
     port: opts.port,
     agent: agent,
-    path: '/' + opts.eh + '/messages?timeout=60&api-version=2014-01',
+    path: "/" + opts.eh + "/messages?timeout=60&api-version=2014-01",
     headers: {
-      Authorization: 'SharedAccessSignature sr=' + opts.sr + '&sig=' + opts.sig + '&se=' + opts.se + '&skn=' + opts.skn,
-      'Content-Type': 'application/atom+xml;type=entry;charset=utf-8'
-    }
-  }
+      Authorization:
+        "SharedAccessSignature sr=" +
+        opts.sr +
+        "&sig=" +
+        opts.sig +
+        "&se=" +
+        opts.se +
+        "&skn=" +
+        opts.skn,
+      "Content-Type": "application/atom+xml;type=entry;charset=utf-8",
+    },
+  };
 
-  const bulkHeaders = Object.assign({}, options.headers,
-    { 'Content-Type': 'application/vnd.microsoft.servicebus.json' })
-  const bulkOptions = Object.assign({}, options,
-    { headers: bulkHeaders })
+  const bulkHeaders = Object.assign({}, options.headers, {
+    "Content-Type": "application/vnd.microsoft.servicebus.json",
+  });
+  const bulkOptions = Object.assign({}, options, { headers: bulkHeaders });
 
-  function callback (done) {
-    return function inner (response) {
-      debug('response.statusCode =', response.statusCode)
-      debug('response.statusMessage =', response.statusMessage)
+  function callback(done) {
+    return function inner(response) {
+      debug("response.statusCode =", response.statusCode);
+      debug("response.statusMessage =", response.statusMessage);
       if (response.statusCode !== 201) {
-        splitter.emit('error', new Error(response.statusCode))
+        splitter.emit("error", new Error(response.statusCode));
       }
 
-      response.on('data', function (data) {
-        debug('data =', data.toString())
-      })
+      response.on("data", function (data) {
+        debug("data =", data.toString());
+      });
 
-      response.on('end', function () {
-        debug('call completed')
-        done()
-      })
-    }
+      response.on("end", function () {
+        debug("call completed");
+        done();
+      });
+    };
   }
 
   const writable = new Writable({
     objectMode: true,
-    highWaterMark: opts['bulk-size'] || 500,
+    highWaterMark: opts["bulk-size"] || 500,
     writev: function (blocks, done) {
       // https://docs.microsoft.com/en-us/rest/api/eventhub/send-batch-events
       const events = blocks
-        .map(block => block.chunk)
-        .map(line => {
+        .map((block) => block.chunk)
+        .map((line) => {
           // check if console output is a string or object
-          const parsed = new Parse(line)
-          return (parsed.err)
+          const parsed = new Parse(line);
+          return parsed.err
             ? JSON.stringify({ Body: line })
-            : `{"UserProperties":${line}}`
+            : `{"UserProperties":${line}}`;
         })
-        .join(',')
-      const body = `[${events}]`
-      debug(`body =`, body)
+        .join(",");
+      const body = `[${events}]`;
+      debug(`body =`, body);
 
-      const req = https.request(bulkOptions, callback(done))
-      req.on('error', (e) => {
-        console.error(`request error =`, e)
-        done()
-      })
-      req.write(body)
-      req.end()
+      const req = https.request(bulkOptions, callback(done));
+      req.on("error", (e) => {
+        console.error(`request error =`, e);
+        done();
+      });
+      req.write(body);
+      req.end();
     },
     write: function (line, enc, done) {
-      debug(`write: line =`, line)
-      debug(`write: typeof ===`, typeof line)
+      debug(`write: line =`, line);
+      debug(`write: typeof ===`, typeof line);
       if (line) {
-        const req = https.request(options, callback(done))
-        req.on('error', (e) => {
-          console.error(`request error =`, e)
-        })
-        req.write(line)
-        req.end()
+        const req = https.request(options, callback(done));
+        req.on("error", (e) => {
+          console.error(`request error =`, e);
+        });
+        req.write(line);
+        req.end();
       } else {
-        done()
+        done();
       }
-    }
-  })
+    },
+  });
 
-  pump(splitter, writable)
+  pipeline(splitter, writable);
 
-  return splitter
+  return splitter;
 }
-module.exports = pinoEventHub
-module.exports.createSignature = createSignature
-module.exports.giveSecurityWarning = giveSecurityWarning
+module.exports = pinoEventHub;
+module.exports.createSignature = createSignature;
+module.exports.giveSecurityWarning = giveSecurityWarning;
--- start.js
+++ start.js
@@ -1,51 +1,72 @@
 #! /usr/bin/env node
-'use strict'
+"use strict";
 
-const minimist = require('minimist')
-const pump = require('pump')
-const fs = require('fs')
-const path = require('path')
-const pinoEventHub = require('./pino-eventhub')
-const socketCount = 10
+const minimist = require("minimist");
+const { pipeline } = require("stream");
+const fs = require("fs");
+const path = require("path");
+const pinoEventHub = require("./pino-eventhub");
+const socketCount = 10;
 
-function start (opts) {
+function start(opts) {
   if (opts.help) {
-    console.log(fs.readFileSync(path.join(__dirname, './usage.txt'), 'utf8'))
-    return
+    console.log(fs.readFileSync(path.join(__dirname, "./usage.txt"), "utf8"));
+    return;
   }
 
   if (opts.version) {
-    console.log('pino-eventhub', require('./package.json').version)
-    return
+    console.log("pino-eventhub", require("./package.json").version);
+    return;
   }
 
-  const ehn = opts['event-hub-namespace'] || process.env.PINO_EVENT_HUB_NAMESPACE
-  const eh = opts['event-hub'] || process.env.PINO_EVENT_HUB
-  const sapn = opts['shared-access-policy-name'] || process.env.PINO_SHARED_ACCESS_POLICY_NAME
-  const sapk = opts['shared-access-policy-key'] || process.env.PINO_SHARED_ACCESS_POLICY_KEY
-  const sas = opts['sas'] || process.env.PINO_SHARED_ACCESS_SIGNATURE
-  const max = opts['max'] || socketCount
+  let eh = opts["event-hub"] || process.env.PINO_EVENT_HUB;
+  let ehn = opts["event-hub-namespace"] || process.env.PINO_EVENT_HUB_NAMESPACE;
+  let sapn =
+    opts["shared-access-policy-name"] ||
+    process.env.PINO_SHARED_ACCESS_POLICY_NAME;
+  let sapk =
+    opts["shared-access-policy-key"] ||
+    process.env.PINO_SHARED_ACCESS_POLICY_KEY;
+  let sas = opts["sas"] || process.env.PINO_SHARED_ACCESS_SIGNATURE;
+
+  const url = opts["url"] || process.env.PINO_CONNECTION_URL;
+  const max = opts["max"] || socketCount;
+
+  if (!ehn || !eh || !sapn) {
+    console.log(fs.readFileSync(path.join(__dirname, "./usage.txt"), "utf8"));
+    console.log(
+      "  1 or more missing required parameters 'event-hub-namespace', 'event-hub', 'shared-access-policy-name' and  'sas'."
+    );
+    if (!sas) {
+      pinoEventHub.giveSecurityWarning();
+    }
+    return;
+  }
 
   if (!ehn || !eh || !sapn || (!sas && !sapk)) {
-    console.log(fs.readFileSync(path.join(__dirname, './usage.txt'), 'utf8'))
-    console.log("  1 or more missing required parameters 'event-hub-namespace', 'event-hub', 'shared-access-policy-name' and  'sas'.")
+    console.log(fs.readFileSync(path.join(__dirname, "./usage.txt"), "utf8"));
+    console.log(
+      "  1 or more missing required parameters 'event-hub-namespace', 'event-hub', 'shared-access-policy-name' and  'sas'."
+    );
     if (!sas) {
-      pinoEventHub.giveSecurityWarning()
+      pinoEventHub.giveSecurityWarning();
     }
-    return
-  }
-  if (opts.expiry && !Number.isNumber(opts.expiry)) {
-    console.log(`"expiry" should be in unix date format`)
-    return
+    return;
   }
 
-  const now = new Date()
-  const week = 60 * 60 * 24 * 7
-  const host = 'https://' + ehn + '.servicebus.windows.net'
+  if (opts.expiry && (typeof opts.expiry !== "number" || isNaN(opts.expiry))) {
+    console.log(`"expiry" should be in unix date format`);
+    return;
+  }
+
+  const week = 7 * 24 * 3600;
+  const host = "https://" + ehn + ".servicebus.windows.net";
   //  path = eh
-  const uri = encodeURIComponent(host + '/' + eh)
-  const se = opts.expiry || process.env.PINO_SAS_EXPIRY ||
-    Math.round(now.getTime() / 1000) + week
+  const uri = encodeURIComponent(host + "/" + eh);
+  const se =
+    opts.expiry ||
+    process.env.PINO_SAS_EXPIRY ||
+    Math.round(Date.now() / 1000) + week;
   const options = Object.assign(opts, {
     host,
     eh,
@@ -53,29 +74,32 @@ function start (opts) {
     sig: sas || pinoEventHub.createSignature(uri, se, sapk, true),
     se,
     skn: sapn,
-    max: max
-  })
+    max: max,
+  });
 
-  pump(process.stdin, pinoEventHub(options))
+  pipeline(process.stdin, pinoEventHub(options));
 }
 
 if (require.main === module) {
-  start(minimist(process.argv.slice(2), {
-    alias: {
-      version: 'v',
-      help: 'h',
-      'event-hub-namespace': 's',
-      'event-hub': 'e',
-      'shared-access-policy-name': 'n',
-      'shared-access-policy-key': 'k',
-      'expiry': 'x',
-      'sas': 'a',
-      'bulk-size': 'b',
-      'port': 'p'
-    },
-    default: {
-      port: 443,
-      'bulk-size': 500
-    }
-  }))
+  start(
+    minimist(process.argv.slice(2), {
+      alias: {
+        version: "v",
+        help: "h",
+        "event-hub-namespace": "s",
+        "event-hub": "e",
+        "shared-access-policy-name": "n",
+        "shared-access-policy-key": "k",
+        expiry: "x",
+        sas: "a",
+        url: "u",
+        "bulk-size": "b",
+        port: "p",
+      },
+      default: {
+        port: 443,
+        "bulk-size": 500,
+      },
+    })
+  );
 }
